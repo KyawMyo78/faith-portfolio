90c466ec8bc5281ffaaf5f2761a4536cc4081a78
KyawMyo78 <kyawmyo.k65@rsu.ac.th>
2025-09-24 19:51:45 +0700

    home: force dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-'use server';
-
+export const dynamic = 'force-dynamic';
 import Navigation from '@/components/Navigation'
 import Hero from '@/components/Hero'
 import Footer from '@/components/Footer'
